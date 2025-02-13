fef80f8337c913eda0f97c7e24dafab16420ff58
karankarhale <karanp.karhale@gmail.com>
2025-02-13 22:26:53 +0530

    Dao Implementation

--- app/src/main/java/com/example/videoapp/database/Room.kt
+++ app/src/main/java/com/example/videoapp/database/Room.kt
@@ -1,1 +1,16 @@
 package com.example.videoapp.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface VideoDao {
+    @Query("select * from DatabaseVideo")
+    fun getVideos(): LiveData<List<DatabaseVideo>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(vararg videos: DatabaseVideo)
+}
