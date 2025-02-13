6fb5e2e3df7ad9d830579d0e3fb20291d55206e1
karankarhale <karanp.karhale@gmail.com>
2025-02-13 22:44:53 +0530

    create room database

--- app/src/main/java/com/example/videoapp/database/Room.kt
+++ app/src/main/java/com/example/videoapp/database/Room.kt
@@ -1,10 +1,14 @@
 package com.example.videoapp.database
 
+import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Database
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Room
+import androidx.room.RoomDatabase
 
 @Dao
 interface VideoDao {
@@ -13,4 +17,21 @@ interface VideoDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(vararg videos: DatabaseVideo)
+}
+
+@Database(entities = [DatabaseVideo::class], version = 1)
+abstract class VideosDatabase : RoomDatabase() {
+    abstract val videoDao: VideoDao
+    private lateinit var INSTANCE: VideosDatabase
+
+    fun getVideosDatabase(context: Context): VideosDatabase {
+        synchronized(VideosDatabase::class.java) {
+            if (!::INSTANCE.isInitialized) {
+                INSTANCE = Room.databaseBuilder(context.applicationContext,
+                    VideosDatabase::class.java,
+                    "videos").build()
+            }
+        }
+        return INSTANCE
+    }
 }
