6fff43de852de56201649b40a829c084d3de5b30
karankarhale <karanp.karhale@gmail.com>
2025-02-14 13:44:35 +0530

    work manager impl

--- app/src/main/java/com/example/videoapp/VideoFeedApplication.kt
+++ app/src/main/java/com/example/videoapp/VideoFeedApplication.kt
@@ -1,9 +1,51 @@
 package com.example.videoapp
 
+import android.annotation.SuppressLint
 import android.app.Application
+import android.os.Build
+import androidx.work.Constraints
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.NetworkType
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import com.example.videoapp.work.RefreshDataWorker
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
 
 class VideoFeedApplication: Application() {
+
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
+
     override fun onCreate() {
         super.onCreate()
+        doInBackgroundThread()
+    }
+
+    private fun doInBackgroundThread() {
+        applicationScope.launch {
+            setRecurringWork()
+        }
+    }
+
+    @SuppressLint("IdleBatteryChargingConstraints")
+    private fun setRecurringWork() {
+        val constraints = Constraints.Builder()
+            .setRequiresCharging(true)
+            .setRequiresBatteryNotLow(true)
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .setRequiresDeviceIdle(true)
+            .build()
+
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(1, TimeUnit.DAYS)
+            .setConstraints(constraints)
+            .build()
+
+        WorkManager.getInstance(applicationContext).enqueueUniquePeriodicWork(
+            RefreshDataWorker.WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            repeatingRequest
+        )
     }
 }
