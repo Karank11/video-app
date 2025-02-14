37cec5ad9e73b5afc7163ff6dc7e6bd257b34848
karankarhale <karanp.karhale@gmail.com>
2025-02-14 12:36:46 +0530

    use of repository

--- app/src/main/java/com/example/videoapp/database/Room.kt
+++ app/src/main/java/com/example/videoapp/database/Room.kt
@@ -22,16 +22,17 @@ interface VideoDao {
 @Database(entities = [DatabaseVideo::class], version = 1)
 abstract class VideosDatabase : RoomDatabase() {
     abstract val videoDao: VideoDao
-    private lateinit var INSTANCE: VideosDatabase
+}
 
-    fun getVideosDatabase(context: Context): VideosDatabase {
-        synchronized(VideosDatabase::class.java) {
-            if (!::INSTANCE.isInitialized) {
-                INSTANCE = Room.databaseBuilder(context.applicationContext,
-                    VideosDatabase::class.java,
-                    "videos").build()
-            }
+private lateinit var INSTANCE: VideosDatabase
+
+fun getVideosDatabase(context: Context): VideosDatabase {
+    synchronized(VideosDatabase::class.java) {
+        if (!::INSTANCE.isInitialized) {
+            INSTANCE = Room.databaseBuilder(context.applicationContext,
+                VideosDatabase::class.java,
+                "videos").build()
         }
-        return INSTANCE
     }
+    return INSTANCE
 }
--- app/src/main/java/com/example/videoapp/viewmodels/VideoFeedViewModel.kt
+++ app/src/main/java/com/example/videoapp/viewmodels/VideoFeedViewModel.kt
@@ -1,6 +1,43 @@
 package com.example.videoapp.viewmodels
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.videoapp.database.getVideosDatabase
+import com.example.videoapp.repository.VideosRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 
-class VideoFeedViewModel: ViewModel() {
+class VideoFeedViewModel(application: Application): AndroidViewModel(application) {
+
+    private val viewModelJob = SupervisorJob()
+    private val viewModelScope = CoroutineScope(viewModelJob + Dispatchers.Main)
+    private val database = getVideosDatabase(application)
+    private val videosRepository = VideosRepository(database)
+
+    init {
+        viewModelScope.launch {
+            videosRepository.refreshVideos()
+        }
+    }
+
+    private val playlist = videosRepository.videos
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+
+    class Factory(val app: Application) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(VideoFeedViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return VideoFeedViewModel(app) as T
+            }
+            throw IllegalArgumentException("Unable to construct viewmodel")
+        }
+    }
 }
